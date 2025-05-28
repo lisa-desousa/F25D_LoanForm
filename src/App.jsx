29c03e63d000ebc3312e1d68a487f2a3b4f30220
lisa-desousa <lisa.desousamestre@gmail.com>
2025-05-28 15:03:46 +0200

    cleanup in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
-import { useState } from "react";
 import "./LoanForm.css";
 import LoanForm from "./Components/LoanForm";
 
 function App() {
-  return (
-    <>
-      <LoanForm />
-    </>
-  );
+  return <LoanForm />;
 }
 
 export default App;
